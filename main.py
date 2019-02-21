93397fca72c3422dedecbc68721b41ca7cf381da
Adamouization <adam@jamour.com>
2019-02-21 15:41:16 +0000

    Read and display images

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    image_path = "dataset/Training/png/001-lighthouse.png"
+    img = mpimg.imread(image_path)
+    imgplot = plt.imshow(img)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
