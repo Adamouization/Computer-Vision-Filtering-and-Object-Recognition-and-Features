39ae656e4bbd7eb0391c7827dcde3c61bb3f9313
Adamouization <adam@jamour.com>
2019-03-07 15:57:03 +0000

    Divide workflow + read dataset dir for images

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 from src.convolution import convolution, gaussian_kernel
+from src.helpers import get_video_filenames
 
 
 def main():
@@ -9,13 +10,24 @@ def main():
     Program entry point.
     :return: None
     """
-    image_directory = "dataset/Training/png/"
-    image_name = "001-lighthouse.png"
+    training_dataset_directory = "dataset/Training/png/"
+    testing_dataset_directory = "dataset/Test/"
 
+    # Blur an image
+    blur_image(training_dataset_directory, "001-lighthouse.png")
+
+    # Train the intensity-based template matching model
+    intensity_based_template_matching_training(training_dataset_directory)
+
+    # Test the intensity-based template matching model
+    intensity_based_template_matching_testing(testing_dataset_directory)
+
+
+def blur_image(directory, image):
     gauss_kernel = gaussian_kernel(10, 10, 5)
     blur_kernel = np.ones((5, 5)) / 25
 
-    img = cv2.imread(image_directory + image_name)
+    img = cv2.imread(directory + image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     conv_img = convolution(gray, gauss_kernel)
@@ -23,6 +35,18 @@ def main():
     cv2.imshow("Original image", gray)
     cv2.imshow("Convoluted image", conv_img)
     cv2.waitKey(0)
+
+
+def intensity_based_template_matching_training(directory):
+    images = get_video_filenames(directory)
+    print("\nTraining image file names:")
+    print(images)
+
+
+def intensity_based_template_matching_testing(directory):
+    images = get_video_filenames(directory)
+    print("\nTesting image file names:")
+    print(images)
 
 
 if __name__ == "__main__":
