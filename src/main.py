53cdf4bb34d61f92b656f6799698e368ef98871c
Adamouization <adam@jamour.com>
2019-03-17 13:59:53 +0000

    Define rotations/scales + write/load binary file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import cv2
 import numpy as np
-from scipy import signal
+from scipy import ndimage, signal
 
 from src.convolution import convolution, gaussian_kernel
 from src.helpers import get_class_name_from_file, get_video_filenames
+
+
+scaling = [512 * s / 20 for s in [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]]
+rotations = [0.0, 30.0, 60.0, 90.0, 120.0, 150.0, 180.0, 210.0, 240.0, 270.0, 300.0, 330.0, 360.0]
 
 
 def main():
@@ -15,13 +19,13 @@ def main():
     testing_dataset_directory = "dataset/Test/"
 
     # Blur an image
-    blur_image(training_dataset_directory, "001-lighthouse.png")
+    # blur_image(training_dataset_directory, "001-lighthouse.png")
 
     # Train the intensity-based template matching model
     intensity_based_template_matching_training(training_dataset_directory)
 
     # Test the intensity-based template matching model
-    intensity_based_template_matching_testing(testing_dataset_directory)
+    # intensity_based_template_matching_testing(testing_dataset_directory)
 
 
 def blur_image(directory, image):
@@ -53,6 +57,18 @@ def intensity_based_template_matching_training(directory):
     for im in images:
         classname = get_class_name_from_file(im)
         print(classname)
+
+    for s in scaling:
+        for r in rotations:
+            print("scaling: {}, rotation: {}".format(s, r))
+
+    img = cv2.imread("/Users/ajaamour/Projects/Computer-Vision-Coursework/dataset/Training/png/001-lighthouse.png")
+    img30 = ndimage.rotate(img, 30.0)
+
+    img30.dump("dataset/Training/templates/test.dat")
+    from_binary = np.load("dataset/Training/templates/test.dat")
+    cv2.imshow("from_binary", from_binary)
+    cv2.waitKey(0)
 
 
 def intensity_based_template_matching_testing(directory):
