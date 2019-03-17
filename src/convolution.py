3f6111782f10175c7ee947c8bcea828efc1e2c35
yissok <andreatv12@gmail.com>
2019-03-17 13:45:10 +0000

    square filters working

--- src/convolution.py
+++ src/convolution.py
@@ -24,6 +24,9 @@ def convolution(image, kernel):
     :param kernel: the kernel used for convolution
     :return: the convoluted image
     """
+    kn = kernel
+
+
     img_height = image.shape[0]
     img_width = image.shape[1]
 
@@ -39,17 +42,64 @@ def convolution(image, kernel):
 
     output_img = np.zeros((img_width, img_height), dtype=np.uint8)
 
-    for i in range(r + 1, img_width - r):
-        for j in range(c + 1, img_height - c):
+    for i in range(r, img_width - r):
+        for j in range(c, img_height - c):
             accumulator = 0
+            #print("kernel[{}, {}] = {}".format(m, n, kernel[m, n]))
+            #print("extended_img[{}, {}] = {}".format(i + m, j + n, extended_img[i + m, j + n]))
+            #print("accumulator = {}".format(accumulator))
+            #print("\n-----\n")
+            for m in range(-r, r+1):
+                for n in range(-c, c+1):
+                    accumulator += kernel[m, n] * extended_img[i + m, j + n]
 
-            for m in range(-r, r):
-                for n in range(-c, c):
-                    accumulator += kernel[m + r + 1, n + c + 1] * extended_img[i + m, j + n]
-
-            output_img[i - r, j - c] = accumulator
+            output_img[i, j] = accumulator
 
     return output_img
+
+
+def reduce_size(image, kernel):
+
+    new_im = image
+
+    img_height = image.shape[0]
+    img_width = image.shape[1]
+
+    kernel_height = kernel.shape[0]
+    kernel_width = kernel.shape[1]
+
+    print(img_height)
+    print(img_width)
+
+    r = int((kernel_width - 1) / 2)
+    c = int((kernel_height - 1) / 2)
+
+    #arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [99, 109, 191, 129]])
+    #print(arr)
+
+    #arr = np.delete(arr, 2, 1)
+    #print(arr)
+
+    output_img = np.zeros((img_width, img_height), dtype=np.uint8)
+
+    for m in range(0, r):
+        new_im=np.delete(new_im, img_width - m - 1, 1)
+    for m in range(0, c):
+        new_im=np.delete(new_im, img_height - m - 1, 0)
+    for m in range(0, r):
+        new_im = np.delete(new_im, 0, 1)
+    for m in range(0, c):
+        new_im = np.delete(new_im, 0, 0)
+
+    img_height = new_im.shape[0]
+    img_width = new_im.shape[1]
+
+    print(img_height)
+    print(img_width)
+    print(r)
+    print(c)
+    return new_im
+
 
 
 def gaussian_kernel(rows, columns, dev=1):
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from scipy import signal
 
-from src.convolution import convolution, gaussian_kernel
+from src.convolution import convolution, gaussian_kernel, reduce_size
 from src.helpers import get_class_name_from_file, get_video_filenames
 
 
@@ -25,23 +25,38 @@ def main():
 
 
 def blur_image(directory, image):
-    gauss_kernel = gaussian_kernel(10, 10, 5)
-    blur_kernel = np.ones((5, 5)) / 25
+    blur_kernel = gaussian_kernel(5, 5, 1) #gauss_kernel
+    #blur_kernel = np.ones((9, 9)) / 81
 
-    img = cv2.imread(directory + image)
+
+
+    img = cv2.imread("/Users/andrealissak/COSE/UNI/SEMESTER 2/Computer-Vision-Coursework/dataset/Training/png/001-lighthouse.png")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     conv_img = convolution(gray, blur_kernel)
 
     cv2.imshow("Original image", gray)
-    cv2.imshow("Convoluted image", conv_img)
 
-    library_conv = signal.convolve2d(gray, blur_kernel, mode="full", boundary="wrap", fillvalue=0)
+
+    library_conv = signal.convolve2d(gray, blur_kernel, mode="full", boundary="fill", fillvalue=0)
     library_conv = library_conv.astype(np.uint8)
 
-    # diff = library_conv - conv_img
-    # print(diff)
+    conv_img_smaller = reduce_size(conv_img, blur_kernel)
+    library_conv_smaller = reduce_size(library_conv, blur_kernel)
+    conv_img_smaller = reduce_size(conv_img_smaller, blur_kernel)
+    library_conv_smaller = reduce_size(library_conv_smaller, blur_kernel)
 
-    cv2.imshow("Library convolution", library_conv)
+    diff = library_conv_smaller - conv_img_smaller
+
+    diff_ori = conv_img - library_conv
+
+    for m in range(0,500,10):
+        myarr = list()
+        for n in range(0,500,10):
+            myarr.append(diff[m,n])
+        print(myarr)
+
+    cv2.imshow("Convoluted image", conv_img_smaller)
+    cv2.imshow("Library convolution", library_conv_smaller)
     cv2.waitKey(0)
 
 
